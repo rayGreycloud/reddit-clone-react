7ac3e81c388768b0909cfa8954f5fd4d7c585bf0
rayGreycloud <rayGreycloud@gmail.com>
2017-08-15 09:39:07 -0700

    Add Firebase to app

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,13 @@
+// App/firebase-config.js
+
+export default {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAB1Y2V092tw58B5tataPzKSO-Kv6udF9s",
+    authDomain: "reddit-clone-e80e3.firebaseapp.com",
+    databaseURL: "https://reddit-clone-e80e3.firebaseio.com",
+    projectId: "reddit-clone-e80e3",
+    storageBucket: "reddit-clone-e80e3.appspot.com",
+    messagingSenderId: "92065243833"
+  };
+};
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,17 @@
 // src/containers/App/index.js
 
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import config from './firebase-config';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    // Initialize firebase
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <div className="App">
