bd875043ef8e8ece58fc5e4ea3e0e9618b129d5f
rayGreycloud <rayGreycloud@gmail.com>
2017-08-15 06:50:52 -0700

    Clean out and restructure boilerplate

---
+++ src/containers/App/index.js
@@ -0,0 +1,15 @@
+// src/containers/App/index.js
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>reddit.clone</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
