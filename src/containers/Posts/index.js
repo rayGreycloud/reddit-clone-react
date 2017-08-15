9372a0bd99aacd76f83d8da827d0b4c3788c9c33
rayGreycloud <rayGreycloud@gmail.com>
2017-08-15 11:14:49 -0700

    Add Posts component

---
+++ src/containers/Posts/index.js
@@ -0,0 +1,27 @@
+// src/containers/Posts/index.js
+
+import React, { Component } from 'react';
+
+class Posts extends Component {
+  render() {
+    // Check loading
+    if (this.props.loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      // Map over data and render
+      <div className="Posts">
+        {this.props.posts.map(post => {
+          return (
+            <div>
+              {post.title}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Posts;
